49831f2537b5f0785645409ffc5663439b29fafb
G65AMG <lcs1997@qq.com>
2018-07-02 12:27:46 +0800

    Site updated: 2018-07-02 12:27:45

--- js/spjx.js
+++ js/spjx.js
@@ -1,8 +1,8 @@
 var data = [
-	{name:"默认解析",url:"http://v.72du.com/api/?url="},
+	{name:"默认解析",url:"http://jiexi.92fz.cn/player/vip.php?url="},
 	{name:"通用解析1",url:"http://jx.aeidu.cn/index.php?url="},
 	{name:"通用解析2",url:"http://vip.thxcw.com/api.php?url="},
-	{name:"通用解析3",url:"http://jiexi.92fz.cn/player/vip.php?url="},
+	{name:"通用解析3",url:"http://http://yun.baiyug.cn/vip/?url="},
 	{name:"通用解析4",url:"http://www.662820.com/xnflv/index.php?url="},
 	{name:"通用解析5",url:"http://api.91exp.com/svip/?url="},
 	{name:"通用解析6",url:"http://vip.thxcw.com/index.php?url="},
